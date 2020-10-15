705f6267078eeb600849b0e4f3cfd7cae642ad23
Ritik Sangwa <ritiksangwan154@gmail.com>
2020-10-15 21:41:10 +0530

    New changes

--- pkg_task0/scripts/node_turtle_revolve.py
+++ pkg_task0/scripts/node_turtle_revolve.py
@@ -6,10 +6,15 @@ from turtlesim.msg import Pose
 
 class Turtle():
     def __init__(self):
-        rospy.init_node('node_turtle_revolve', anonymous=True)  #Initializing Ros node
-        
+        ################## Initializing Ros Node #################################################
+        rospy.init_node('node_turtle_revolve', anonymous=True)                        
+
+        ################## Publishing node for turtle1/cmd_vel topic ############################
         self.data_pub=rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+
+        #################  Subscribing node for turtle1/pose topic  ###############################
         rospy.Subscriber("turtle1/pose", Pose, self.getPose)
+
 
         self.iniTime = rospy.get_time()
         self.rate    = rospy.Rate(10)
@@ -22,11 +27,12 @@ class Turtle():
         self.axisMovement.angular.y = 0.0
         self.axisMovement.angular.z = 1.0
         self.theta = 0.00
-
-
+        
+    #################### Callback function for Subscribing topic #############################
     def getPose(self,data):
         self.theta = data.theta
 
+    #################### Publishing Stopping values #########################################
     def stopValue(self):
         self.stopValue = Twist()
         self.stopValue.linear.x = 0.0
@@ -40,10 +46,13 @@ class Turtle():
         self.data_pub.publish(self.stopValue)
         rospy.loginfo('Goal Reached')
 
+
+    ####################### Publishing to topic #################################################
     def rotation(self):
         self.time = rospy.get_time()-self.iniTime
         self.data_pub.publish(self.axisMovement)
         rospy.loginfo('Moving in Circle: '+str(self.time))
+
 
 
 if __name__ == '__main__':
@@ -51,12 +60,11 @@ if __name__ == '__main__':
        rotate_tur = Turtle()
 
        while not rospy.is_shutdown():
-        rate = rospy.Rate(10)
-        rate.sleep()
+        rotate_tur.rate.sleep()
         rotate_tur.rotation()
         if(rotate_tur.theta<0.0 and rotate_tur.theta>-0.15):
             rotate_tur.stopValue()
-            break
+            rospy.signal_shutdown("Goal Reached")
 
     except rospy.ROSInterruptException:
         pass
